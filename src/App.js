92f166689873acd5fcd6562894e079200eeadc75
AbhirajSrivastava <abhiraj201199@gmail.com>
2023-08-27 18:18:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AttendanceTable from './AttendanceTable';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Attendence System</h1>
+      <AttendanceTable />
+    </div>
+  );
+}
+
+export default App;
